2b9b6d337d8485bc8fdae2ce61e599f2d540c697
Lorenzo-77 <lorenzo.geraldo.munoz@gmail.com>
2026-06-04 12:46:42 -0300

    1.5

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { CartProvider } from './components/context/CartContext';
 import { Navbar } from './components/layout/Navbar';
 import { CartDrawer } from './components/layout/CartDrawer';
